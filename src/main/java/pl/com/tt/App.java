1ac0c0297852ded40ccb664be6a50c41e0cef101
Krzysztof Tumicki <KrzysztofTumicki27@gmail.com>
2016-03-15 16:57:23 +0100

    pom and basic source

---
+++ src/main/java/pl/com/tt/App.java
@@ -0,0 +1,22 @@
+package pl.com.tt;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static Logger logger = LogManager.getLogger(App.class.getName());
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        logger.info("application started!");
+        
+        
+        logger.info("application end!");
+    }
+}
